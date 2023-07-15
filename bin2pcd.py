179edee3575906e2bd41b8374b575bcf9c3fcfd3
Fazlı Batuhan Karataş <karatas.fazlibatuhan@student.atilim.edu.tr>
2023-07-15 12:47:22 +0300

    converting multiple bin files to pcd files

--- bin2pcd.py
+++ bin2pcd.py
@@ -1,28 +1,32 @@
 import numpy as np
 import struct
-import sys
-import open3d as o3d
+from open3d import *
+import os
 
-
-def bin_to_pcd(binFileName):
+def convert_kitti_bin_to_pcd(binFilePath):
     size_float = 4
     list_pcd = []
-    with open(binFileName, "rb") as f:
+    with open(binFilePath, "rb") as f:
         byte = f.read(size_float * 4)
         while byte:
             x, y, z, intensity = struct.unpack("ffff", byte)
             list_pcd.append([x, y, z])
             byte = f.read(size_float * 4)
     np_pcd = np.asarray(list_pcd)
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(np_pcd)
+    pcd = open3d.geometry.PointCloud()
+    pcd.points = open3d.utility.Vector3dVector(np_pcd)
     return pcd
 
-def main(binFileName, pcdFileName):
-    pcd = bin_to_pcd(binFileName)
-    o3d.io.write_point_cloud(pcdFileName, pcd)
+#bin file directory
+path_of_the_directory= "binFiles"
 
-if __name__ == "__main__":
-    a = sys.argv[1]
-    b = sys.argv[2]
-    main(a, b)
+for filename in os.listdir(path_of_the_directory):
+
+    bin_file = os.path.join(path_of_the_directory,filename)
+
+    if os.path.isfile(bin_file):
+        print(bin_file[-10:-4])
+        # convert
+        pcd_ = convert_kitti_bin_to_pcd(bin_file)
+        # save
+        open3d.io.write_point_cloud("pcdFiles\\" + bin_file[-10:-4] + '.pcd', pcd_, write_ascii=False, compressed=False, print_progress=False)
